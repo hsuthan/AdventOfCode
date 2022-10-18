d975a85852d29cef4f8bcafeca4e6d4a24103533
Harishaan Suthan <harishaan.suthan@slu.edu>
2022-10-18 05:14:46 -0500

    working solution

---
+++ SonarSweep.java
@@ -0,0 +1,33 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class SonarSweep
+{
+    public static void main(String []args)
+    {
+        File inputFile = new File("input.txt");
+        Scanner scanner;
+
+        try
+        {
+            scanner = new Scanner(inputFile);
+            int previousDepth = 0, counter = -1;
+            while (scanner.hasNextInt())
+            {
+                int currentDepth = scanner.nextInt();
+                if (currentDepth > previousDepth)
+                {
+                    counter ++;
+                }
+                previousDepth = currentDepth;
+            }
+
+            System.out.println(counter);
+        }
+        catch (FileNotFoundException error)
+        {
+            System.out.println("ERROR" + error.getMessage());
+        }
+    }
+}
